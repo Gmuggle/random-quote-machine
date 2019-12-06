4efa00293e69eddbee579db0e40cf345c35ac0fd
Gmuggle <you@example.com>
2019-12-06 15:57:02 +0800

    Updates

---
+++ precache-manifest.f7236a454ecf16888758a40df9f295f7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a971c49c51f78a7ad2e2bf74ad7ecf1",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "90d7800941b57332343b",
+    "url": "/random-quote-machine/static/css/2.ae19aa97.chunk.css"
+  },
+  {
+    "revision": "0353ee31048b38791df3",
+    "url": "/random-quote-machine/static/css/main.54cf6238.chunk.css"
+  },
+  {
+    "revision": "90d7800941b57332343b",
+    "url": "/random-quote-machine/static/js/2.37f4a96c.chunk.js"
+  },
+  {
+    "revision": "0353ee31048b38791df3",
+    "url": "/random-quote-machine/static/js/main.855a61f2.chunk.js"
+  },
+  {
+    "revision": "26230a9c3f6e526bba8c",
+    "url": "/random-quote-machine/static/js/runtime-main.e239717d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/random-quote-machine/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/random-quote-machine/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/random-quote-machine/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/random-quote-machine/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/random-quote-machine/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/random-quote-machine/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
